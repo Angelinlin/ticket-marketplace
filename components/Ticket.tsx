c8014be521c469b000ab76eda6e779d994d335cb
Angelinlin <angel.muncerv@gmail.com>
2024-12-06 16:01:01 -0600

    update currency symbol from GBP to MXN in event-related components

--- components/Ticket.tsx
+++ components/Ticket.tsx
@@ -112,7 +112,9 @@ export default function Ticket({ ticketId }: { ticketId: Id<"tickets"> }) {
               />
               <div>
                 <p className="text-sm text-gray-500">Ticket Price</p>
-                <p className="font-medium">£{ticket.event.price.toFixed(2)}</p>
+                <p className="font-medium">
+                  MXN{ticket.event.price.toFixed(2)}
+                </p>
               </div>
             </div>
           </div>
