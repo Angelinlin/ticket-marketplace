ef490a29227fdc45d65f5401b2008210153c0144
Angelinlin <angel.muncerv@gmail.com>
2024-12-06 16:14:14 -0600

    update currency symbol to MXN in createStripeCheckoutSession and add remote hostname to next.config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    // This should accept all the local hostnames and the remote hostnames
+    // This should also accept all the protocols https://brave-coyote-725.convex.cloud/api/storage/6699e468-ace5-42a6-af4b-2a65c46af33e
     remotePatterns: [
       { hostname: "upbeat-stoat-959.convex.cloud", protocol: "https" },
       { hostname: "wary-anaconda-29.convex.cloud", protocol: "https" },
+      { hostname: "brave-coyote-725.convex.cloud", protocol: "https" }, // Agrega esta línea
+      
     ],
   },
 };
